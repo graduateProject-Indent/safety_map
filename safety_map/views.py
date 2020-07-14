df6beed0e95ddbab5d247350bf6ad3c596ab3d05
hanjeongwon97@gmail.com <hanjeongwon97@gmail.com>
2020-07-14 17:01:50 +0900

    todo : marker st_x(), st_y() error

--- safety_map/views.py
+++ safety_map/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from safety_map.models import SafetyZone
 import folium
 import urllib.request
 import datetime
@@ -6,7 +7,8 @@ import time
 import json
 import webbrowser
 import pymysql
-
+import numpy as np
+import pandas as pd
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
@@ -20,7 +22,6 @@ def showMaps(request):
     return render(request, 'home.html',{'map':maps})
 
 #한정원 : 안심장소보기 미완성
-from safety_map.models import SafetyZone
 
 def showSafetyZone(request):
     map = folium.Map(location=[37.6511988,127.0161604],zoom_start=12)
@@ -30,11 +31,9 @@ def showSafetyZone(request):
 def showszMarker(request):
     map = folium.Map(location=[37.6511988,127.0161604],zoom_start=12)
     folium.Marker([37.566345, 126.977893],popup='testtest').add_to(map)
-    i = 1 
-    p = SafetyZone.objects.get(safety_zone_pk=i)
-    for i in range(100):
-        folium.Marker([37.566345, 126.977893],popup='testtest').add_to(map)
-        i = i+1
-
+    #han = SafetyZone.objects.all()
+    for i in range(1,101):
+        han = SafetyZone.objects.get(safety_zone_pk=i)
+        folium.Marker([ST_X(han.safety_loc), ST_Y(han.safety_loc)],popup='testtest').add_to(map)
     maps = map._repr_html_()
     return render(request,'safetyzone.html',{'map':maps})
