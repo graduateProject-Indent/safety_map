aa49ae2e5766869f947c401361841405faf4b70c
sooahkim <saligo96@gmail.com>
2020-08-04 13:22:27 +0900

    pathFinder function

--- safety_map/views.py
+++ safety_map/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,get_object_or_404
+from django.http import HttpResponse,JsonResponse
 from .models import *
 import folium
 import binascii
@@ -16,12 +17,7 @@ from plpygis import Geometry
 import branca
 
 g = geocoder.ip('me')
-mid=g.latlng
-top_left=[mid[1]-0.008,mid[0]+0.008]
-bottom_left=[mid[1]+0.008,mid[0]+0.008]
-bottom_right=[mid[1]+0.008,mid[0]-0.008]
-top_right=[mid[1]-0.008,mid[0]-0.008]
-polyNY_shapely = Polygon([(top_left), (bottom_left), (bottom_right), (top_right)])
+
 
 # Create your views here.
 def home(request):
@@ -37,12 +33,11 @@ def showMaps(request):
     return render(request, 'home.html',{'map':maps})
 
 def showFemale(request):
-    global mid
-    global polyNY_shapely
     crime_type=""
     if request.method=="POST":
         filter_value=request.POST['female_filter']
         crime_type="전체_"+filter_value
+    
     female_total=Female2.objects.filter(female2_crime_type=crime_type).all()
     loc_list=[]
     for loc in female_total:
@@ -50,17 +45,15 @@ def showFemale(request):
         to_geojson=convert.wkt_to_geojson(str(gis.shapely))
         to_coordinate=json.loads(to_geojson)
         contain_coordinate=shape(to_coordinate)
-        if polyNY_shapely.contains(contain_coordinate):
-            crime_location={"type":"Feature","geometry":to_coordinate}
-            loc_list.append(crime_location)
-        
+        crime_location={"type":"Feature","geometry":to_coordinate}
+        loc_list.append(crime_location)
     pistes = {"type":"FeatureCollection","features":loc_list}
     #print(pistes)
-    style = {'fillColor': '#DC143C', 'lineColor': '#00FFFFFF'}
+    #style = {'fillColor': '#DC143C', 'lineColor': '#00FFFFFF'}
     map = folium.Map(location=mid,zoom_start=15)
-    folium.GeoJson(pistes,style_function=lambda x: style).add_to(map)
+    folium.GeoJson(pistes).add_to(map)
     maps=map._repr_html_()
-    return render(request, 'home.html',{'map':maps})
+    return render(request, 'female2.html',{'map':maps,'pistes':pistes})
 
 #한정원 : 안심장소 안전벨 테스트 100개 
 def filter_safetyzone_bell(request): # 한정원
@@ -93,3 +86,17 @@ def danger_map(request):
 def register_danger(request):
     return render(request, 'register_danger.html')
 
+def pathFinder(request):   
+    female_total=Female2.objects.filter(female2_crime_type="전체_전체").all()
+    loc_list=[]
+    for loc in female_total:
+        gis= Geometry(loc.female2_crime_loc.hex()[8:])
+        to_geojson=convert.wkt_to_geojson(str(gis.shapely))
+        to_coordinate=json.loads(to_geojson)
+        contain_coordinate=shape(to_coordinate)
+        crime_location={"type":"Feature","geometry":to_coordinate}
+        loc_list.append(crime_location)
+    pistes = str({"type":"FeatureCollection","features":loc_list})
+    return render(request,'female2.html',{'pistes':pistes})
+
+    
