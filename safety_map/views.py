8cd032de14b87582912cf624578f7e58bc16e937
hanjeongwon97@gmail.com <hanjeongwon97@gmail.com>
2020-08-06 13:12:37 +0900

    안심장소 보기 구 선택해서 출력하는것 제외하고 모두 완료(마커까지)

--- safety_map/views.py
+++ safety_map/views.py
@@ -62,9 +62,9 @@ def showFemale(request):
 
 
 def filter_safetyzone(request): #안심장소보기
-
     safety_type = ""
     gu_type = ""
+    mkurl = ""
     map = folium.Map(location=[37.55582994870823, 126.9726320033982],zoom_start=12)
 
     if request.method=="POST":
@@ -73,18 +73,28 @@ def filter_safetyzone(request): #안심장소보기
 
     # 편의점을 선택한 경우 선택된 구를 출력
     if(safety_type=="편의점") : 
+        mkurl = "safety_map/static/img/mk_cvs.png" #편의점 마커 이미지
         safetyzone_ob_all = SafetyZone.objects.filter(gu='종로구') # 구 입력 방식 정해지면 '종로구'자리에 gu_type 넣으면 된다.
     # 경찰서, 지구대, 파출소를 선택한 경우 서울 전체
     else : 
+        if(safety_type=="경찰서"):
+            mkurl = "safety_map/static/img/mk_police_station.png" # 경찰서 마커 이미지
+        elif(safety_type=="지구대"):
+            mkurl = "safety_map/static/img/mk_police_unit.png" # 지구대 마커 이미지
+        elif(safety_type=="파출소"):
+            mkurl = "safety_map/static/img/mk_police_box.png" # 파출소 마커 이미지
+
         safetyzone_ob_all = SafetyZone.objects.filter(safety_type=safety_type).all()
-    
+
     # 마커 추가
+    
     for loc in safetyzone_ob_all:
+        icon = folium.features.CustomIcon(icon_image=mkurl,icon_size=(50,50))
         gis = Geometry(loc.safety_loc.hex()[8:])
         to_geojson=convert.wkt_to_geojson(str(gis.shapely))
         to_coordinate=json.loads(to_geojson)
-        folium.Marker([to_coordinate['coordinates'][0],to_coordinate['coordinates'][1]],popup='hello',
-            icon=folium.Icon(color='green')).add_to(map)
+        marker = folium.map.Marker([to_coordinate['coordinates'][0],to_coordinate['coordinates'][1]],icon=icon)
+        marker.add_to(map)
 
     maps=map._repr_html_()
     return render(request,'home.html',{'map':maps})
